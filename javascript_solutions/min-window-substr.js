cebf5e5644df51ef6e94c125b99f411f7c015a04
Bruno C da Silva <bcdasilv@calpoly.edu>
2022-10-16 11:40:30 -0700

    Updated and solved min-window-substr Leetcode hard

--- javascript_solutions/min-window-substr.js
+++ javascript_solutions/min-window-substr.js
@@ -6,73 +6,76 @@
  * @return {string}
  */
  var minWindow = function(s, t) {
-    /*
-        Not the optimal solution but it solves the problem.
+    
+    if (t.length > s.length) return "";
 
-        1) build a map of char frequency for t 
-            key is the char and value is # of times it appears on t
-            O(size of t)
-            
-        2) Iterate over s and t (nested loops)
-        Start with a char in s and pick a substr which is the length of t
-        if that substr can be a possible solution save it and start the outter
-        loop again with the next char (sliding to the right)
-        If that substr cannot be a possible solution, increase the string by picking
-        the next character (increasing the window/substr until it picks a solution or hits the end)
-    */
+    if (t.length === s.length && s === t) return s;
+
+    const t_map = buildMap(t);
     
-    const t_map = new Map();
-    for (let i = 0; i < t.length; i++) {
-        const t_freq = t_map.get(t.charAt(i));
-        if (t_freq) {
-            t_map.set(t.charAt(i), t_freq+1);
-        } else {
-            t_map.set(t.charAt(i), 1);
-        }       
-    } 
-    
+    let i = 0;
+    let j = i + t.length; //start window with size of t
     let result = "";
-    for (let i = 0; i < s.length; i++) {
-        for (let j = i + t.length; j <= s.length; j++) { //note the <= since the slice doesn't include the upper bound index
-            const substr = s.slice(i,j);
-            // console.log(substr)
-            if (contains(substr, t_map)) {
-                j = s.length + 1;//start a new slide window
-                if (result === "" || result.length > substr.length) {
-                     result = substr;
-                    // console.log(`result: ${result}`);
-                } 
-            }            
+    const s_map = buildMap(s.slice(i, t.length));
+
+    while (j <= s.length && i < j) {
+        const substr = s.slice(i, j);
+        if (contains(s_map, t_map)) {
+            if (result === "" || substr.length < result.length) {
+                result = substr;
+            }
+            //left pointer moves forward
+            decreasingMap(s_map, s.charAt(i)); // update the s_map
+            i++; //move pointer
+        } else {
+            //right pointer moves forward
+            if (j < s.length) {
+                increasingMap(s_map, s.charAt(j)); // minus 1 because we're using j in the slice function that doesn't include j
+            }
+            j++;
         }
     }
-    return result;
+   return result;
 };
 
-// var contains = function(substr, t) {
-//     for (let i = 0; i < t.length; i++) {
-//         const t_char = t.charAt(i);
-//         if (!substr.includes(t_char))
-//             return false;
-//     }
-//     return true;
-// }
-
-var contains = function(substr, t_map) {    
-    const substr_map = new Map();
-    for (let i = 0; i < substr.length; i++) {
-        const substr_freq = substr_map.get(substr.charAt(i));
-        if (substr_freq) {
-            substr_map.set(substr.charAt(i), substr_freq+1);
+var buildMap = function (str) {
+    const map = new Map();
+    for (let i = 0; i < str.length; i++) {
+        const freq = map.get(str.charAt(i));
+        if (freq) {
+            map.set(str.charAt(i), freq+1);
         } else {
-            substr_map.set(substr.charAt(i), 1);
-        }    
+            map.set(str.charAt(i), 1);
+        }       
+    } 
+    return map;
+}
+
+var increasingMap = function(s_map, char) {   
+    let freq = 0;
+    if (s_map.has(char)) {
+        freq = s_map.get(char);
     }
-        
+    s_map.set(char, freq + 1);
+}
+
+var decreasingMap = function(s_map, char) {   
+    if (s_map.has(char)) {
+        const freq = s_map.get(char);
+        if (freq === 1)
+            s_map.delete(char);
+        else 
+            s_map.set(char, freq - 1);
+    }
+}
+
+var contains = function(s_map, t_map) {   
     for (t_entry of t_map) {
         const t_char = t_entry[0];
         const t_freq = t_entry[1];
-        if (!substr_map.get(t_char) || substr_map.get(t_char) < t_freq)
+        if (!s_map.get(t_char) || s_map.get(t_char) < t_freq)
             return false;
-    } 
+    }
+    
     return true;
 }
